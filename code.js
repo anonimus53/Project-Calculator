1e01178ca0a92f8f8069a2288cf72f16fa2d4ee3
anonimus53 <futapas@yahoo.com>
2023-07-09 17:32:38 +0300

    display function not done yet

--- code.js
+++ code.js
@@ -22,7 +22,7 @@ function divide(a,b){
     console.log(quotient)
     return quotient;
 }
-let firstNum ;
+let firstNumfu ;
 let secondNum ;
 let operator ;
 //choose right oporation
@@ -41,3 +41,23 @@ function operate(operator, firstNum, secondNum){
     }
 }
 //buttons functionality
+
+//current display of screen
+let preDisplay = 0;
+//get screen
+const screen = document.getElementById('screen');
+//display the number alone if zero is present or the number with the existing
+//ex: screen has already 1 and you press 2, you get 12
+function display(item){
+ if(typeof item == 'number' && preDisplay == 0){
+    console.log(item);
+
+ }else if(typeof item == 'number' && preDisplay != 0){
+    console.log(preDisplay.toString() + item.toString());
+ }else if(typeof item != 'number' && preDisplay != 0){
+    //i need to display the minus sign if '-/+' is pressed or remove it.
+    //but what if '=' is pressed, then we need to display the result
+    // and if others like '+' are pressed similary
+    // if 'AC' is pressed then we just clear everything
+ }
+}
